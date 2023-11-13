8d90c548cc8d4f9d0e4d994ab7345871eb1e661d
ponyo19 <tominhduy09@gmail.com>
2023-11-13 22:24:35 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+#include <SFML/Main.hpp>
+#include <SFML/System.hpp>
+#include <iostream>
+
+#include "Blah.h"
+
+int main() {
+  std::cout << "Hello World" << std::endl;
+  Blah().boo();
+  sf::Int32 blah;
+  return 0;
+}
---
+++ src/Blah.cpp
@@ -0,0 +1,5 @@
+#include "Blah.h"
+
+#include <iostream>
+
+void Blah::boo() { std::cout << "Boo!\n"; }
